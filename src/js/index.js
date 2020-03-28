348e8239034e8048be7428080d20077c11994f14
Kurt Guardia <kurtguardia@gmail.com>
2020-03-28 22:17:32 +0100

    Added the module to render Recipes on the UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import { elements, renderLoader, clearLoader } from './views/base';
-import Recipe from './models/Recipe';
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import * as recipeView from "./views/recipeView";
+import { elements, renderLoader, clearLoader } from "./views/base";
+import Recipe from "./models/Recipe";
 
-
-/** Global state of the app 
+/** Global state of the app
  * - Search objetc
  * -Current recipe object
  * - shopping list object
  * - Liked recipes
-*/
+ */
 const state = {};
 
 /**
@@ -17,85 +17,84 @@ const state = {};
  */
 
 const controlSearch = async () => {
-    
-        //1) Get query from the view
-    const query = searchView.getInput();
+  //1) Get query from the view
+  const query = searchView.getInput();
 
-    if (query) {
+  if (query) {
+    //2) New search object and add to stage
+    state.search = new Search(query); //Brought from the exported class
 
-        //2) New search object and add to stage
-        state.search = new Search(query)            //Brought from the exported class
+    //3) Prepare UI for results
+    searchView.clearInput(); //To erase the input field
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-        //3) Prepare UI for results
-        searchView.clearInput();                    //To erase the input field
-        searchView.clearResults();
-        renderLoader(elements.searchRes)
+    try {
+      //4) Search for recipes
+      await state.search.getResults(); //This method returns a Promise and in step 5) we need the results to be compleated, that's why we need to wait this method to be done
 
-        try{
-        //4) Search for recipes
-        await state.search.getResults();            //This method returns a Promise and in step 5) we need the results to be compleated, that's why we need to wait this method to be done
-
-        //5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);        //state.search.result is an array that we get thanks to the method getResults(), and contains all the results of the search. We pass it to the renderResults that we made in the other module
-        
-        } catch (error) {
-            alert('Something went wrong :(')
-            clearLoader();
-        }
+      //5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result); //state.search.result is an array that we get thanks to the method getResults(), and contains all the results of the search. We pass it to the renderResults that we made in the other module
+    } catch (error) {
+      alert("Something went wrong :(");
+      clearLoader();
     }
-}
+  }
+};
 
-elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
-})
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
 
-elements.searchResPages.addEventListener('click', e => {        //searchResPages is the parent of where we put the buttons
-    const btn = e.target.closest('.btn-inline');                //.closest gives us the element with the specified class closest to where we clicked (so no problem on clicking the letters or the icon inside the button, we always get the button)
+elements.searchResPages.addEventListener("click", e => {
+  //searchResPages is the parent of where we put the buttons
+  const btn = e.target.closest(".btn-inline"); //.closest gives us the element with the specified class closest to where we clicked (so no problem on clicking the letters or the icon inside the button, we always get the button)
 
-    if(btn){
-        const goToPages = parseInt(btn.dataset.goto, 10);       //goto is the dataset we made for the respective button, so it tells us what that button is made for
-        searchView.clearResults();                              //So the new results don't drop off under the first results, and also erase the button we just clicked
-        searchView.renderResults(state.search.result, goToPages);       //Same as step 5 in controlSearch, but with the new page attached as the second argument
-    }
-})
+  if (btn) {
+    const goToPages = parseInt(btn.dataset.goto, 10); //goto is the dataset we made for the respective button, so it tells us what that button is made for
+    searchView.clearResults(); //So the new results don't drop off under the first results, and also erase the button we just clicked
+    searchView.renderResults(state.search.result, goToPages); //Same as step 5 in controlSearch, but with the new page attached as the second argument
+  }
+});
 
 /**
- * Recipe stuff (front of browser) 
+ * Recipe stuff (front of browser)
  */
-const  controlRecipe = async() => {
-    //Get ID from the url
-    const id = window.location.hash.replace('#', '');
-    console.log(id);
+const controlRecipe = async () => {
+  //Get ID from the url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
 
-    //Pretty much do everythinh
-    if(id) {
+  //Pretty much do everythinh
+  if (id) {
+    
+    //Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+    //Create new recipe object
+    state.recipe = new Recipe(id);
 
-        //Prepare UI for changes
+    try {
+      //Get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
 
-        //Create new recipe object
-        state.recipe = new Recipe(id);
+      //Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServing();
 
-        try {
-        //Get recipe data
-        await state.recipe.getRecipe();
-
-        //Calculate servings and time
-        state.recipe.calcTime();
-        state.recipe.calcServing();
-
-        //Render recipe
-        console.log(state.recipe);
-
-        } catch (error) {
-            alert('Something went wrong :(')
-        }
+      //Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (error) {
+      //alert("Something went wrong :(");
+      console.log(error);
     }
-}
+  }
+};
 
-
-
-
-
-['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+["hashchange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -25,14 +25,74 @@ export default class Recipe {
         const numIng = this.ingredients.length;
         const periods = Math.ceil(numIng / 3);
         this.time = periods * 15; 
-    };
+    }
 
     calcServing() {
         //No actual reason for this number, it's just four.
         this.servings = 4;
     }
 
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'onunce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];         //All of the measures that we can get and DON'T want
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];                                          //The measures we want and will replace the ones above if necesary
+        const units = [...unitsShort, 'kg', 'g'];
 
+        const newIngredients = this.ingredients.map(el => {             //to apply this to all elements inside the 
+
+            //Uniform measures
+            let ingredients = el.toLowerCase();                         //To avoid Capital letters prblemes
+            unitsLong.forEach((unit, i) => {                            //We search fot the long versions
+                ingredients = ingredients.replace(unit, unitsShort[i])  //in the ingredients we replace the ones that look alike the long measures with the equivalent in short version, using the index of the long in the short
+            })
+
+            //Remove parentesis
+            ingredients = ingredients.replace(/ *\([^)]*\) */g, ' ');   //to remove parenthesis
+
+            //Parse ingredients
+            const arrIng = ingredients.split(' ');                      //To store all ingredients in an Array, individually
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));        //To store where the in the Array (index) you can get the units (tbsp, oz, cup, etc)
+
+            let objIng;                   //Main object
+            
+            if(unitIndex > -1){
+                //There is a unit
+                const arrCount = arrIng.slice(0, unitIndex);            //For us to know if there's one or two elements in the array that are the number (4 is just one but 4 1/3 are two elements than mean one)
+                let count;
+                
+                if(arrCount.length === 1){
+                    count = eval(arrIng[0].replace('-', '+'));        //if theres a - in there, we switch it to a +, and then evaluate the sum (4-1/30 => 4+1/3 => 4.333)
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));   //If theres a space between numbers we join them with a + and then evaluate the sum (4 1/3 => 4+1/3 => 4.333)
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredients: arrIng.slice(unitIndex + 1).join(' ')      //joinin Array elements with a space, after the measure unit
+                }
+                
+            } else if (parseInt(arrIng[0], 10)){
+                //no unit but 1st is a number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredients: arrIng.slice(1).join(' ')                  //joinin Array elements with a space, after the first element
+                }
+
+            } else if (unitIndex === -1){
+                //no unit nor number
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredients
+                }
+            }
+
+            return objIng;
+        });
+
+        this.ingredients = newIngredients
+    }
 
 
 
